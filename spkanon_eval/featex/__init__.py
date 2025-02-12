91fb411dfeafb5887f5b8ecf1e052bbe4b0440f7
Carlos (HPC) <carlos.franzreb@dfki.de>
2025-02-12 09:35:35 +0100

    Add prosody embedding

--- spkanon_eval/featex/__init__.py
+++ spkanon_eval/featex/__init__.py
@@ -6,5 +6,6 @@ from .asr.wav2vec import Wav2Vec  # noqa
 from .asr.whisper_wrapper import Whisper  # noqa
 from .spkid.spkid import SpkId  # noqa
 from .spkid.spkid_concat import SpkIdConcat  # noqa
+from .spkid.prosody_embedding import ProsodyEmbedding  # noaq
 from .wavlm.wrapper import WavlmWrapper  # noqa
 from .spectrogram import SpecExtractor  # noqa

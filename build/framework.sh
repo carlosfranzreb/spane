b81dba9422a30512a27eda677ceb9a324793875b
Carlos Franzreb <>
2025-07-24 16:22:54 +0200

    Fix scripts

--- build/framework.sh
+++ build/framework.sh
@@ -1,6 +1,6 @@
-# ! This script should be called from the parent folder of the repository
-
-# create and activate conda environment
+#! /bin/bash
+# Create a conda environment with the framework
+# This script should be called from the parent folder of the repository
 conda update -n base -c defaults conda
 conda create -p ./venv python=3.10 -y
 eval "$(conda shell.bash hook)" && conda activate ./venv
--- build/run_tests.sh
+++ build/run_tests.sh
@@ -1,5 +1,6 @@
-# ! This script should be called from the parent folder of the repository
-# run tests with coverage check
+#! /bin/bash
+# Runs tests with coverage check
+# This script should be called from the parent folder of the repository
 coverage run --omit=./spkanon_eval/NISQA/*,./spkanon_eval/spkanon_eval/featex/wavlm/modules.py,./spkanon_eval/spkanon_eval/featex/wavlm/wavlm_model.py -m unittest discover -s spkanon_eval/tests -p "test_*.py"
 coverage combine -m
 coverage report -m --include="spkanon_eval/*"

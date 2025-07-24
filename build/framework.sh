7a54fc8b2928fa270c2b8993d27fcd29ce06ec3f
Carlos Franzreb <>
2025-07-24 08:53:57 +0200

    Fix build script

--- build/framework.sh
+++ build/framework.sh
@@ -2,7 +2,7 @@
 
 # create and activate conda environment
 conda update -n base -c defaults conda
-conda create -p ./venv python=3.11 -y
+conda create -p ./venv python=3.10 -y
 eval "$(conda shell.bash hook)" && conda activate ./venv
 
 # ffmpeg is required to load MP3 files
@@ -10,12 +10,11 @@ conda install -y 'ffmpeg<5'
 
 # install pip dependencies
 pip install --no-input --upgrade pip
-pip install --no-input ./spkanon_eval
+pip install --no-input -e ./spkanon_eval
 
 # clone NISQA
-cd spkanon_eval
-git clone https://github.com/gabrielmittag/NISQA.git
-cd ..
+git clone https://github.com/gabrielmittag/NISQA.git ./spkanon_eval/NISQA
+export PYTHONPATH=$(pwd)/spkanon_eval:$PYTHONPATH
 
 # run tests
-python -m unittest discover -s ./spkanon_eval/tests -p "test_*.py"
+python -m unittest discover -s spkanon_eval/tests -p "test_*.py"

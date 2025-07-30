f0935c03e33d0e2a80ffaf378c80bb38eb121c9a
Carlos Franzreb <>
2025-07-30 11:17:32 +0200

    Update paths after renaming repository

--- build/run_tests.sh
+++ build/run_tests.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 # Runs tests with coverage check
 # This script should be called from the parent folder of the repository
-coverage run --omit=./spane/NISQA/*,./spane/spkanon_eval/featex/wavlm/modules.py,./spane/spkanon_eval/featex/wavlm/wavlm_model.py -m unittest discover -s spkanon_eval/tests -p "test_*.py"
+coverage run --omit=./spane/NISQA/*,./spane/spkanon_eval/featex/wavlm/modules.py,./spane/spkanon_eval/featex/wavlm/wavlm_model.py -m unittest discover -s spane/tests -p "test_*.py"
 
 if [ $? -eq 1 ]; then
-    message="[Policy] Unit tests failed, please check and fix your code."
+    message="Unit tests failed, please check and fix your code."
     echo -e "\033[1;31mERROR: $message\033[0m";
     exit 1
 else
-    message="[Policy] Passed unit tests."
+    message="Passed unit tests."
     echo -e "\033[1;32mOK: $message\033[0m"
     coverage combine
     coverage report -m --include="spane/*"

4bccfa9044a0d17edc1388410c18b10ad10f1505
Carlos (HPC) <carlos.franzreb@dfki.de>
2025-09-09 11:12:35 +0200

    Load ckpt before fine-tuning

--- spkanon_eval/featex/spkid/spkid.py
+++ spkanon_eval/featex/spkid/spkid.py
@@ -13,7 +13,7 @@ import shutil
 from speechbrain.inference.speaker import EncoderClassifier
 from speechbrain.utils.checkpoints import Checkpointer
 from hyperpyyaml import load_hyperpyyaml
-from omegaconf import OmegaConf
+from omegaconf import DictConfig
 import torch
 
 from .train import SpeakerBrain, prepare_dataset
@@ -24,13 +24,15 @@ LOGGER = logging.getLogger("progress")
 
 
 class SpkId(InferComponent):
-    def __init__(self, config: OmegaConf, device: str) -> None:
+    def __init__(self, config: DictConfig, device: str) -> None:
         """
         Initialize the model with the given config and freeze its parameters.
 
         The model is usually initialized with pre-trained weights. If a checkpoint is
         set in the config, it will be loaded afterwards. If the input dimension to the
-        encoder is different than 80, which is the expected size of the 
+        encoder is different than 80, which is the expected size of the pre-trained
+        model, the model will be initialized with the train config, whose size should
+        match the checkpoint.
         """
         self.sample_rate = 16000
         self.config = config
@@ -40,51 +42,58 @@ class SpkId(InferComponent):
             source=config.path, savedir=self.save_dir, run_opts={"device": device}
         )
         if config.get("ckpt", None) is not None:
-            LOGGER.info(f"Loading checkpoint {config.ckpt}")
-            emb_model_ckpt = os.path.join(config.ckpt, "embedding_model.ckpt")
-            emb_model_state_dict = torch.load(
-                emb_model_ckpt, map_location=device, weights_only=False
-            )
-            ckpt_dim = emb_model_state_dict["blocks.0.conv.conv.weight"].shape[1]
-            model_dim = self.model.mods.embedding_model.blocks[0].conv.in_channels
-
-            # if the shape does not fit, initialize the model with the train config
-            if ckpt_dim != model_dim:
-                with open(config.train_config) as f:
-                    hparams = load_hyperpyyaml(
-                        f,
-                        overrides={
-                            "output_folder": ".",
-                            "out_n_neurons_src": 1,
-                            "out_n_neurons_tgt": 1,
-                            "num_workers": 1,
-                            "n_epochs_zero": 1,
-                            "n_epochs_max": 1,
-                            "max_weight": 1,
-                        },
-                    )
-
-                speaker_brain = SpeakerBrain(
-                    modules=hparams["modules"],
-                    hparams=hparams,
-                    run_opts={"device": self.device},
-                )
-                self.model.mods.embedding_model = speaker_brain.modules.embedding_model
-                self.model.mods.compute_features = speaker_brain.modules.compute_features
-            
-        # load the checkpoints
-        self.model.mods.embedding_model.load_state_dict(emb_model_state_dict)
-
-        compute_features_ckpt = os.path.join(config.ckpt, "compute_features.ckpt")
-        if os.path.exists(compute_features_ckpt):
-            compute_features_state_dict = torch.load(
-                    compute_features_ckpt, map_location=device, weights_only=False
-                )
-            self.model.mods.compute_features.load_state_dict(compute_features_state_dict)
-        else:
-            LOGGER.warning("There is no `compute_features.ckpt`")
+            self.load_ckpt(self.model, config.ckpt, config.train_config)
 
         self.model.eval()
+    
+    def load_ckpt(self, model: EncoderClassifier, ckpt_dir: str, train_config_f: str):
+        """Load the speechbrain checkpoint.
+        For speaker_brain (train), embedding_models was accessed by hparams.
+        TODO: test that this works for init and train.
+        """
+        LOGGER.info(f"Loading checkpoint {ckpt_dir}")
+        emb_model_ckpt = os.path.join(ckpt_dir, "embedding_model.ckpt")
+        emb_model_state_dict = torch.load(
+            emb_model_ckpt, map_location=self.device, weights_only=False
+        )
+        ckpt_dim = emb_model_state_dict["blocks.0.conv.conv.weight"].shape[1]
+        model_dim = model.hparams.embedding_model.blocks[0].conv.in_channels
+
+        # if the shape does not fit, initialize the model with the train config
+        if ckpt_dim != model_dim:
+            with open(train_config_f) as f:
+                hparams = load_hyperpyyaml(
+                    f,
+                    overrides={
+                        "output_folder": ".",
+                        "out_n_neurons_src": 1,
+                        "out_n_neurons_tgt": 1,
+                        "num_workers": 1,
+                        "n_epochs_zero": 1,
+                        "n_epochs_max": 1,
+                        "max_weight": 1,
+                    },
+                )
+
+            speaker_brain = SpeakerBrain(
+                modules=hparams["modules"],
+                hparams=hparams,
+                run_opts={"device": self.device},
+            )
+            model.hparams.embedding_model = speaker_brain.modules.embedding_model
+            model.hparams.compute_features = speaker_brain.modules.compute_features
+        
+        # load the checkpoints
+        model.hparams.embedding_model.load_state_dict(emb_model_state_dict)
+
+        compute_features_ckpt = os.path.join(ckpt_dir, "compute_features.ckpt")
+        if os.path.exists(compute_features_ckpt):
+            compute_features_state_dict = torch.load(
+                    compute_features_ckpt, map_location=self.device, weights_only=False
+                )
+            model.hparams.compute_features.load_state_dict(compute_features_state_dict)
+        else:
+            LOGGER.warning("There is no `compute_features.ckpt`")
 
     def to(self, device: str) -> None:
         self.device = device
@@ -204,11 +213,7 @@ class SpkId(InferComponent):
 
         # if a ckpt is given, it should be fine-tuned
         if self.config.get("ckpt", None) is not None:
-            LOGGER.info(f"Fine-tuning emb. model {self.config.ckpt}")
-            state_dict = torch.load(
-                self.config.ckpt, map_location=self.device, weights_only=False
-            )
-            speaker_brain.hparams.embedding_model.load_state_dict(state_dict)
+            self.load_ckpt(speaker_brain, self.config.ckpt, self.config.train_config)
 
         speaker_brain.epoch_losses = {"TRAIN": [], "VALID": []}
         val_kwargs = hparams["dataloader_options"].copy()
@@ -244,8 +249,8 @@ class SpkId(InferComponent):
         )
 
         # load the trained model
-        self.model.mods.embedding_model = speaker_brain.modules.embedding_model
-        self.model.mods.compute_features = speaker_brain.modules.compute_features
+        self.model.hparams.embedding_model = speaker_brain.modules.embedding_model
+        self.model.hparams.compute_features = speaker_brain.modules.compute_features
         self.model.eval()
 
 
